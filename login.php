3972ac0fd58a8e3f9a6b750247c2930def7e4994
Kevince05 <cesco.kevin05@gmail.com>
2024-05-04 13:06:49 +0200

    setup login + register

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if ($db->connect_error) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST["email"];
-    $pwd = md5($_POST["pwd"]);
+    $pwd = md5($_POST["password"]);
 
     if ($_POST["submit_type"] === "Login") {
         $prep = $db->prepare("SELECT * FROM users WHERE email=? AND password=?");
@@ -18,17 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $result = $prep->get_result();
         if ($result->num_rows == 1) {
             $_SESSION["email"] = $email;
-            $_SESSION["pwd"] = $pwd;
+            $_SESSION["password"] = $pwd;
             $_SESSION["admin"] = $result->fetch_assoc()["admin"] === 1 ? true : false;
             header("Location:index.php");
         } else {
             $error = "Wrong user or password";
         }
     } else {
-        $prep = $db->prepare("INSERT INTO users (email, password) VALUE (?,?)");
-        $prep->bind_param("ss", $email, $pwd);
+        $name = $_POST["name"];
+        $surname = $_POST["surname"];
+        $prep = $db->prepare("INSERT INTO users (email, password, name, surname, last_login) VALUE (?,?,?,?, CURDATE())");
+        $prep->bind_param("ssss", $email, $pwd, $name, $surname);
         if ($prep->execute()) {
-            header("Location:ilogin.php");
+            header("Location:index.php");
         } else {
             $error = "Error:" . $db->error;
         }
@@ -51,20 +53,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <section>
         <div class="container-fluid">
             <div class="row">
-                <div class="col"<?php $col=['#0dcaf0','#20c997','#198754','#fd7e14','#6f42c1','#6610f2','#0d6efd']; echo 'style = "background-color:'. $col[array_rand($col,1)] .'50;"'?> id="loginCol">
+                <div class="col" <?php $col = ['#0dcaf0', '#20c997', '#198754', '#fd7e14', '#6f42c1', '#6610f2', '#0d6efd'];
+                                    echo 'style = "background-color:' . $col[array_rand($col, 1)] . '50;"' ?> id="loginCol">
                     <div class="d-flex position-relative top-50 start-50 translate-middle">
-                        <form class="w-100">
+                        <form method="post" class="w-100">
                             <h3 class="fw-normal mb-3 pb-3">Log in</h3>
-                            <div class="mb-3">
+                            <div id="name-wrapper" class="mb-3" style="display: none">
+                                <label class="form-label" for="name">Name</label>
+                                <input name="name" type="text" id="name" class="form-control form-control" />
+                            </div>
+                            <div id="surname-wrapper" class="mb-3" style="display: none">
+                                <label class="form-label" for="surname">Surname</label>
+                                <input name="surname" type="surname" id="surname" class="form-control form-control" />
+                            </div>
+                            <div id="email-wrapper" class="mb-3">
                                 <label class="form-label" for="email">Email address</label>
-                                <input type="email" id="email" class="form-control form-control"/>
+                                <input name="email" type="email" id="email" class="form-control form-control" required />
                             </div>
-                            <div class="mb-3">
+                            <div id="password-wrapper" class="mb-3">
                                 <label class="form-label" for="password">Password</label>
-                                <input type="password" id="password" class="form-control form-control"/>
+                                <input name="password" type="password" id="password" class="form-control form-control" required />
                             </div>
-                            <input type="submit" name="submit_type" value="Login" class="btn btn-primary mt-1" />
-                            <input type="submit" name="submit_type" value="Register" class="btn btn-primary mt-1"/>
+                            <div id="submit-wrapper">
+                                <input id="login" type="submit" name="submit_type" value="Login" class="btn btn-primary mt-1" />
+                                <input id="register" type="button" name="submit_type" value="Register" class="btn btn-primary mt-1" />
+                                <script>
+                                    let register = document.getElementById("register");
+                                    let login = document.getElementById("login");
+                                    register.addEventListener("click", function() {
+                                        document.getElementById("name-wrapper").style.display = "block";
+                                        document.getElementById("name").required = true;
+                                        document.getElementById("surname-wrapper").style.display = "block";
+                                        document.getElementById("surname").required = true;
+                                        register.type = "submit";
+                                        login.value = "Cancel";
+                                        login.classList.remove("btn-primary");
+                                        login.classList.add("btn-danger");
+                                    })
+
+                                    login.addEventListener("click", function() {
+                                        if (login.value === "Cancel") {
+                                            document.getElementById("name-wrapper").style.display = "none";
+                                            document.getElementById("name").required = false;
+                                            document.getElementById("surname-wrapper").style.display = "none";
+                                            document.getElementById("surname").required = false;
+                                            register.type = "button";
+                                            login.value = "Login";
+                                            login.classList.remove("btn-danger");
+                                            login.classList.add("btn-primary");
+                                        }
+                                    })
+                                </script>
+                            </div>
+                            <?php
+                            if (isset($error)) {
+                                echo '<div id="error-wrapper" class="container"><p>' . $error . '</p></div>';
+                            }
+                            ?>
                         </form>
                     </div>
                 </div>
@@ -76,11 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         </div>
     </section>
-    <?php
-    if (isset($error)) {
-        echo '<div class="error-container"><p>' . $error . '</p></div>';
-    }
-    ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </html>
